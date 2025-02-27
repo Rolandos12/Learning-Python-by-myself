f4cf96f7a0f3ff15acc1aef9fe65bae1fe7f08e1
Rolandos12 <163935309+Rolandos12@users.noreply.github.com>
2025-02-27 18:28:19 +0200

    Add files via upload
    day 3

---
+++ curs udemy d3.py
@@ -0,0 +1,73 @@
+# print("welcome to the rollercoaster")
+# height=int(input("What is your height?: "))
+# if height>=120:
+#     print("You have the necessary height. How old are you?")
+#     age=int(input("My age is: "))
+#     if age<=12:
+#         print("You are a tiny kid have to pay 5$")
+#     elif age<=18:
+#         print("You are a little boy need to pay 7$")
+#     elif age>=18:
+#         print("You are a growth man and have to pay 12$")
+# else:
+#     print("You don't have the required height.")
+
+#Write a program that interprets the Body Mass Index (BMI) based on a user's weight and height.
+# It should tell them the interpretation of their BMI based on the BMI value.
+#     Under 18.5 they are underweight
+#     Over 18.5 but below 25 they have a normal weight
+#     Over 25 but below 30 they are overweight
+#     Over 30 but below 35 they are obese
+#     Above 35 they are clinically obese.
+
+
+#print("Welcome to BMI interpreter!")
+## weight=float(input("What is your weight: "))
+## height=float(input("Waht is your height? "))
+## bmi=round(weight/height**2)
+# bmi= 42
+# print(bmi)
+# if bmi <18.5:
+#     print("You are underweight")
+# elif bmi<25:
+#     print("You have a normal weight")
+# elif bmi<30:
+#     print("You are are overweight")
+# elif bmi<35:
+#     print("You are obese")
+# else:
+#     print("You are clinically obese.")
+
+
+# LOVE calculator exercise
+
+# print("Welcome to the TRUE LOVE Calculator. Please enter your name down below:")
+# name1 = input("My name is: ")
+# name2 = input("2. My name is: ")
+#
+# x= str(name1.lower())
+# y= str(name2.lower())
+#
+# t= x.count("t") + y.count("t")
+# r= x.count("r") + y.count("r")
+# u= x.count("u") + y.count("u")
+# e= x.count("e") + y.count("e")
+#
+# l= x.count("l") + y.count("l")
+# o= x.count("o") + y.count("o")
+# v= x.count("v") + y.count("v")
+# e2= x.count("e") + y.count("e")
+#
+# true= t+r+u+e
+# love= l+o+v+e2
+#
+# truelove= int(str(true)+ str(love))
+# if truelove <10 or truelove>90:
+#     print(f'Your score is {truelove}, you go together like coke and mentos.')
+# elif truelove >40 and truelove<50:
+#     print(f'Your score is {truelove}, you are alright together.')
+# else: print(f'Your score is {truelove} you are ok')
+#
+#
+
+
